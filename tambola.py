17eb12a2ee4519e1b360c2cc735967bfc0433ff6
iam-shanmukha <vishnushanmukha@gmail.com>
2020-08-19 16:35:24 +0530

    added comments

--- tambola.py
+++ tambola.py
@@ -6,6 +6,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout 
 from kivy.graphics import Color
 from kivy.clock import Clock
+from kivy.uix.slider import Slider
 import random
 coins = random.sample(range(1,91), 90)
 picked_coins=[0]
@@ -13,7 +14,9 @@ class Housie(FloatLayout):
 	def __init__(self,**kwargs):
 		super(Housie,self).__init__(**kwargs)
 		self.title = Label(text="Housie Coin Picker",font_size = 50,size_hint=(1, .55),pos_hint={'x':0, 'y':.60})
+		#Label to show Picked Number
 		self.main_label = Label(text = "",font_size=90, size_hint=(1, .60),pos_hint={'x':0, 'y':.40})
+		#Label to show previous number
 		self.prev_label = Label(text = "Previous Number",font_size=30, size_hint=(1, .60),pos_hint={'x':0, 'y':.25})
 		#self.picked_ones = Label(text = "picked_coins", size_hint=(1, .40),pos_hint={'x':0, 'y':.40})
 		self.help_button = Button(text = "PICK NUMBER", size_hint=(.3, .1),pos_hint={'x':.65, 'y':.1},on_press = self.update)
@@ -21,9 +24,12 @@ class Housie(FloatLayout):
 		self.add_widget(self.main_label)
 		self.add_widget(self.prev_label)
 		#self.add_widget(self.picked_ones)
+
 		self.add_widget(self.help_button)
 		self.add_widget(self.userinterface())
-		Clock.schedule_interval(self.update, 2)
+
+		#Enabel below Picks coin based on Time automatically
+		#Clock.schedule_interval(self.update, 2) 
 
 	def userinterface(self):
 		self.layout = GridLayout(cols = 10,size_hint=(.50, .50))
@@ -39,6 +45,7 @@ class Housie(FloatLayout):
 				for i in self.layout.children:
 					if i.text == str(coin):
 						print(i,i.text)
+						#change the color of picked coin
 						i.background_normal=''
 						i.background_color=(0,0,1,1)
 						#print(i,i.text)
