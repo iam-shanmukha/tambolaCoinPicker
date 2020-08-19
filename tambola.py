d78f3be7f976c6432dd6260392112362ccaf996d
iam-shanmukha <vishnushanmukha@gmail.com>
2020-08-19 13:42:13 +0530

    added previous num

--- tambola.py
+++ tambola.py
@@ -7,20 +7,19 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.graphics import Color
 import random
 coins = random.sample(range(1,91), 90)
-#print(coins)
-picked_coins=[]
-current_coin=0
-#print(picked_coins)
+picked_coins=[0]
 class Housie(FloatLayout):
 	def __init__(self,**kwargs):
 		super(Housie,self).__init__(**kwargs)
-		self.title = Label(text="Housie Coin Picker",font_size = 50,size_hint=(1, .55),pos_hint={'x':0, 'y':.45})
-		self.main_label = Label(text = "Click PICK NUMBER", size_hint=(1, .60),pos_hint={'x':0, 'y':.35})
-		self.picked_ones = Label(text = "picked_coins", size_hint=(1, .40),pos_hint={'x':0, 'y':.40})
+		self.title = Label(text="Housie Coin Picker",font_size = 50,size_hint=(1, .55),pos_hint={'x':0, 'y':.60})
+		self.main_label = Label(text = "",font_size=90, size_hint=(1, .60),pos_hint={'x':0, 'y':.40})
+		self.prev_label = Label(text = "Previous Number",font_size=30, size_hint=(1, .60),pos_hint={'x':0, 'y':.25})
+		#self.picked_ones = Label(text = "picked_coins", size_hint=(1, .40),pos_hint={'x':0, 'y':.40})
 		self.help_button = Button(text = "PICK NUMBER", size_hint=(.3, .1),pos_hint={'x':.65, 'y':.1},on_press = self.update)
 		self.add_widget(self.title)
 		self.add_widget(self.main_label)
-		self.add_widget(self.picked_ones)
+		self.add_widget(self.prev_label)
+		#self.add_widget(self.picked_ones)
 		self.add_widget(self.help_button)
 		self.add_widget(self.userinterface())
 	def userinterface(self):
@@ -31,7 +30,7 @@ class Housie(FloatLayout):
 	def update(self,event):
 		for coin in coins:
 			if coin not in picked_coins:
-				current_coin=coin
+				self.prev_label.text = str(f"Previous Number: {picked_coins[-1]}")
 				picked_coins.append(coin)
 				self.main_label.text = str(coin)
 				for i in self.layout.children:
@@ -39,9 +38,9 @@ class Housie(FloatLayout):
 						print(i,i.text)
 						i.background_normal=''
 						i.background_color=(0,0,1,1)
-						print(i,i.text)
+						#print(i,i.text)
 				break
-		self.picked_ones.text = "Picked coins = {}".format(" ".join(str(sorted(picked_coins))))
+		#self.picked_ones.text = "Picked coins = {}".format(" ".join(str(sorted(picked_coins))))
 class app1(App):
 	def build(self):
 		return Housie()
